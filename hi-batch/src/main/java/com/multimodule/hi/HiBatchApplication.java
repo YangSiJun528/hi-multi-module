035e30956cdef52bd45fa61a0ceedfa1c87d3071
YangSijun528 <yangsijun5528@gmail.com>
2023-05-22 20:32:32 +0900

    first commit

---
+++ hi-batch/src/main/java/com/multimodule/hi/HiBatchApplication.java
@@ -0,0 +1,13 @@
+package com.multimodule.hi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiBatchApplication.class, args);
+    }
+
+}
---
+++ hi-core/src/test/java/com/multimodule/hi/core/HiCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multimodule.hi.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
