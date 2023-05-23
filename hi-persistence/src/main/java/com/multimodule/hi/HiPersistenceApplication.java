bfd3eeea589647d4e384592ae09934683c61a41a
YangSijun528 <yangsijun5528@gmail.com>
2023-05-23 15:49:28 +0900

    init

---
+++ hi-persistence/src/main/java/com/multimodule/hi/HiPersistenceApplication.java
@@ -0,0 +1,13 @@
+package com.multimodule.hi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiPersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiPersistenceApplication.class, args);
+	}
+
+}
