b8c72eb4a6c704706a34d77d07a4b94b0f2521b2
YangSijun528 <yangsijun5528@gmail.com>
2023-05-24 16:18:35 +0900

    [persistence] PostRepository 오타 수정

--- hi-persistence/src/main/java/com/multimodule/hi/domain/persistence/post/repository/PostRepository.java
+++ hi-persistence/src/main/java/com/multimodule/hi/domain/persistence/post/repository/PostRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     Optional<Post> findByTitle(String title);
-    boolean existPByTitle(String title);
+    boolean existsByTitle(String title);
 }
